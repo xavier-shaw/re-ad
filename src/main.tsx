9e08e7c4ae1022ef9b548e7677e0ccb601dd105f
brianroysar <brian.sinambela1@gmail.com>
2025-03-04 20:11:22 -0800

    Before callback

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.tsx'
+
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+
+import { createRoot } from "react-dom/client";
+import { App } from "./App";
+
+// biome-ignore lint/style/noNonNullAssertion: Root element must be there
+const container = document.getElementById("root")!;
+const root = createRoot(container);
+root.render(<App />);
