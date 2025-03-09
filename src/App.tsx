29f64a5d3dc27c732e431d6da5f8ea8fd39bbb7a
Michael Shi <69874869+mykulls@users.noreply.github.com>
2025-03-08 23:58:24 -0800

    jank starter nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
-
-
 import "./App.css";
-import "./../node_modules/react-pdf-highlighter/dist/style.css"
+import "./../node_modules/react-pdf-highlighter/dist/style.css";
 import GraphPanel from "./containers/GraphPanel";
 import PaperPanel from "./containers/PaperPanel";
 import { Box } from "@mui/material";
 import { PaperContextProvider } from "./contexts/PaperContext";
+import NavBar from "./components/NavBar";
 
 function App() {
-
   return (
     <PaperContextProvider>
       <div className="App">
-        <Box className="panel paper-panel">
-          <PaperPanel />
-        </Box>
-        <Box className="panel graph-panel">
-          <GraphPanel />
-        </Box>
+        <header>
+          <NavBar />
+        </header>
+        <div className="content">
+          <Box className="panel paper-panel">
+            <PaperPanel />
+          </Box>
+          <Box className="panel graph-panel">
+            <GraphPanel />
+          </Box>
+        </div>
       </div>
     </PaperContextProvider>
   );
--- src/contexts/PaperContext.tsx
+++ src/contexts/PaperContext.tsx
@@ -1,145 +1,162 @@
 import { createContext, useCallback, useEffect, useState } from "react";
 import { Content, IHighlight, NewHighlight, ScaledPosition } from "react-pdf-highlighter";
-import { type Node, type Edge, useNodesState, useEdgesState, type OnNodesChange, type OnEdgesChange, addEdge, Connection, MarkerType } from "@xyflow/react";
+import {
+  type Node,
+  type Edge,
+  useNodesState,
+  useEdgesState,
+  type OnNodesChange,
+  type OnEdgesChange,
+  addEdge,
+  Connection,
+  MarkerType,
+} from "@xyflow/react";
+
+export interface ColoredHighlight extends NewHighlight {
+  color: string;
+}
+
+export interface ColoredIHighlight extends IHighlight {
+  color?: string;
+}
 
 type PaperContextData = {
-    highlights: Array<IHighlight>
-    addHighlight: (highlight: NewHighlight) => void
-    updateHighlight: (highlightId: string, position: Partial<ScaledPosition>, content: Partial<Content>) => void
-    resetHighlights: () => void
-    // Graph
-    nodes: Array<Node>
-    setNodes: (nodes: Array<Node>) => void
-    onNodesChange: OnNodesChange
-    edges: Array<Edge>
-    setEdges: (edges: Array<Edge>) => void
-    onEdgesChange: OnEdgesChange
-    onConnect: (connection: Connection) => void
-    // Shared
-    selectedHighlightId: string | null
-    setSelectedHighlightId: (highlightId: string | null) => void
-}
+  highlights: Array<ColoredIHighlight>;
+  addHighlight: (highlight: ColoredHighlight) => void;
+  updateHighlight: (highlightId: string, position: Partial<ScaledPosition>, content: Partial<Content>) => void;
+  resetHighlights: () => void;
+  // Graph
+  nodes: Array<Node>;
+  setNodes: (nodes: Array<Node>) => void;
+  onNodesChange: OnNodesChange;
+  edges: Array<Edge>;
+  setEdges: (edges: Array<Edge>) => void;
+  onEdgesChange: OnEdgesChange;
+  onConnect: (connection: Connection) => void;
+  // Shared
+  selectedHighlightId: string | null;
+  setSelectedHighlightId: (highlightId: string | null) => void;
+  currentColor: string | null;
+  setCurrentColor: (color: string) => void;
+};
 
 export const PaperContext = createContext<PaperContextData | null>(null);
 
 export const PaperContextProvider = ({ children }: { children: React.ReactNode }) => {
-    const [highlights, setHighlights] = useState<Array<IHighlight>>([]);
-    const [readId, setReadId] = useState(0);
-    const [temporalSeq, setTemporalSeq] = useState(0);
+  const [highlights, setHighlights] = useState<Array<ColoredIHighlight>>([]);
+  const [readId, setReadId] = useState(0);
+  const [temporalSeq, setTemporalSeq] = useState(0);
 
-    // Shared
-    const [selectedHighlightId, setSelectedHighlightId] = useState<string | null>(null);
+  // Shared
+  const [selectedHighlightId, setSelectedHighlightId] = useState<string | null>(null);
 
-    // Graph
-    const [nodes, setNodes, onNodesChange] = useNodesState([] as Node[]);
-    const [edges, setEdges, onEdgesChange] = useEdgesState([] as Edge[]);
-    const NODE_OFFSET_X = 150;
-    const NODE_OFFSET_Y = 150;
+  // Graph
+  const [nodes, setNodes, onNodesChange] = useNodesState([] as Node[]);
+  const [edges, setEdges, onEdgesChange] = useEdgesState([] as Edge[]);
+  const NODE_OFFSET_X = 150;
+  const NODE_OFFSET_Y = 150;
 
-    const onConnect = useCallback((connection: Connection) => {
-        console.log("Connect", connection);
-        const edge = { ...connection, type: 'relation' };
-        setEdges(prevEdges => addEdge(edge, prevEdges));
-    }, []);
+  const [currentColor, setCurrentColor] = useState<string | null>(null);
 
-    useEffect(() => {
-        setTemporalSeq(highlights.filter(h => h.id.startsWith(readId.toString())).length);
-    }, [readId]);
+  const onConnect = useCallback((connection: Connection) => {
+    console.log("Connect", connection);
+    const edge = { ...connection, type: "relation" };
+    setEdges((prevEdges) => addEdge(edge, prevEdges));
+  }, []);
 
-    const addHighlight = (highlight: NewHighlight) => {
-        console.log("Add highlight", highlight);
-        setHighlights((prevHighlights: Array<IHighlight>) => [
-            ...prevHighlights,
-            {
-                ...highlight,
-                id: `${readId}-${temporalSeq}`,
-            },
-        ]);
-        setTemporalSeq(prevTemporalSeq => prevTemporalSeq + 1);
+  useEffect(() => {
+    setTemporalSeq(highlights.filter((h) => h.id.startsWith(readId.toString())).length);
+  }, [readId]);
 
-        // add a node to the graph
-        const isFirstHighlight = (temporalSeq === 0);
-        setNodes((prevNodes: Array<Node>) => [
-            ...prevNodes,
-            {
-                id: `${readId}-${temporalSeq}`,
-                type: 'highlight',
-                data: {
-                    id: `${readId}-${temporalSeq}`,
-                    label: highlight.content.text,
-                    content: highlight.content.text,
-                },
-                position: {
-                    x: isFirstHighlight ? readId * NODE_OFFSET_X : nodes[nodes.length - 1].position.x,
-                    y: isFirstHighlight ? NODE_OFFSET_Y : nodes[nodes.length - 1].position.y + NODE_OFFSET_Y,
-                },
-            },
-        ]);
+  const addHighlight = (highlight: ColoredHighlight) => {
+    console.log("Add highlight", highlight);
+    setHighlights((prevHighlights: Array<ColoredIHighlight>) => [
+      ...prevHighlights,
+      {
+        ...highlight,
+        id: `${readId}-${temporalSeq}`,
+      },
+    ]);
+    setTemporalSeq((prevTemporalSeq) => prevTemporalSeq + 1);
 
-        // add an edge to the graph
-        if (!isFirstHighlight) {
-            setEdges((prevEdges: Array<Edge>) => [
-                ...prevEdges,
-                {
-                    id: `${readId}-${temporalSeq}`,
-                    source: highlights[highlights.length - 1]?.id,
-                    target: `${readId}-${temporalSeq}`,
-                    type: 'temporal',
-                    markerEnd: { type: MarkerType.Arrow },
-                },
-            ]);
-        }
-    };
+    // add a node to the graph
+    const isFirstHighlight = temporalSeq === 0;
+    setNodes((prevNodes: Array<Node>) => [
+      ...prevNodes,
+      {
+        id: `${readId}-${temporalSeq}`,
+        type: "highlight",
+        data: {
+          id: `${readId}-${temporalSeq}`,
+          label: highlight.content.text,
+          content: highlight.content.text,
+        },
+        position: {
+          x: isFirstHighlight ? readId * NODE_OFFSET_X : nodes[nodes.length - 1].position.x,
+          y: isFirstHighlight ? NODE_OFFSET_Y : nodes[nodes.length - 1].position.y + NODE_OFFSET_Y,
+        },
+      },
+    ]);
 
-    const updateHighlight = (
-        highlightId: string,
-        position: Partial<ScaledPosition>,
-        content: Partial<Content>,
-    ) => {
-        console.log("Update highlight", highlightId, position, content);
-        setHighlights((prevHighlights: Array<IHighlight>) =>
-            prevHighlights.map((h) => {
-                const {
-                    id,
-                    position: originalPosition,
-                    content: originalContent,
-                    ...rest
-                } = h;
-                return id === highlightId
-                    ? {
-                        id,
-                        position: { ...originalPosition, ...position },
-                        content: { ...originalContent, ...content },
-                        ...rest,
-                    }
-                    : h;
-            }),
-        );
-    };
+    // add an edge to the graph
+    if (!isFirstHighlight) {
+      setEdges((prevEdges: Array<Edge>) => [
+        ...prevEdges,
+        {
+          id: `${readId}-${temporalSeq}`,
+          source: highlights[highlights.length - 1]?.id,
+          target: `${readId}-${temporalSeq}`,
+          type: "temporal",
+          markerEnd: { type: MarkerType.Arrow },
+        },
+      ]);
+    }
+  };
 
-    const resetHighlights = () => {
-        setHighlights([]);
-    };
-
-    return (
-        <PaperContext.Provider value={{
-            highlights,
-            addHighlight,
-            updateHighlight,
-            resetHighlights,
-            // Graph
-            nodes,
-            setNodes,
-            onNodesChange,
-            edges,
-            setEdges,
-            onEdgesChange,
-            onConnect,
-            // Shared
-            selectedHighlightId,
-            setSelectedHighlightId,
-        }}>
-            {children}
-        </PaperContext.Provider>
+  const updateHighlight = (highlightId: string, position: Partial<ScaledPosition>, content: Partial<Content>) => {
+    console.log("Update highlight", highlightId, position, content);
+    setHighlights((prevHighlights: Array<ColoredIHighlight>) =>
+      prevHighlights.map((h) => {
+        const { id, position: originalPosition, content: originalContent, ...rest } = h;
+        return id === highlightId
+          ? {
+              id,
+              position: { ...originalPosition, ...position },
+              content: { ...originalContent, ...content },
+              ...rest,
+            }
+          : h;
+      })
     );
+  };
+
+  const resetHighlights = () => {
+    setHighlights([]);
+  };
+
+  return (
+    <PaperContext.Provider
+      value={{
+        highlights,
+        addHighlight,
+        updateHighlight,
+        resetHighlights,
+        // Graph
+        nodes,
+        setNodes,
+        onNodesChange,
+        edges,
+        setEdges,
+        onEdgesChange,
+        onConnect,
+        // Shared
+        selectedHighlightId,
+        setSelectedHighlightId,
+        currentColor,
+        setCurrentColor,
+      }}
+    >
+      {children}
+    </PaperContext.Provider>
+  );
 };
