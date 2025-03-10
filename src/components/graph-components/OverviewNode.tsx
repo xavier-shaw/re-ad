cddbec294d4ea87c2dc0a74feaf32ec48dfe84f1
brianroysar <brian.sinambela1@gmail.com>
2025-03-10 16:00:34 -0700

    compatible with overviewNode also

--- src/components/graph-components/OverviewNode.tsx
+++ src/components/graph-components/OverviewNode.tsx
@@ -3,11 +3,15 @@ import { Box } from "@mui/material";
 import "../../styles/OverviewNode.css";
 
 export default function OverviewNode({ data }: NodeProps<Node>) {
-    const { id, label, content } = data as { id: string, label: string, content: string };
+    const { id, label, content, isImage} = data as { id: string, label: string, content: string, isImage: boolean};
     return (
         <Box className="overview-node" id={`node-${id}`}>
             <Handle type="target" position={Position.Top} />
-            <h6>{label}</h6>
+            {isImage ? (
+                <img src={label} alt="Node Content" style={{ maxWidth: "100%", maxHeight: "100px" }} />
+            ) : (
+                <h6>{label}</h6>
+            )}
             <p>{content}</p>
             <Handle type="source" position={Position.Bottom} />
         </Box>
